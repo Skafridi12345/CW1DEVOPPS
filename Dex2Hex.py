336570857370332d4aa8371f6f1a3b8c3be2fb25
balwntsng143 <b313@caledonian.ac.uk>
2025-03-17 13:28:07 +0000

    replace the variable name

--- Dex2Hex.py
+++ Dex2Hex.py
@@ -3,12 +3,12 @@ import sys
 def decimal_to_hex(decimal_value): 
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'] 
     hexadecimal = "" 
-    num = decimal_value 
+    number = decimal_value 
     print(f"Converting the Decimal Value {num} to Hex...") 
-    while num != 0: 
-        rem = num % 16 
+    while number != 0: 
+        rem = number % 16 
         hexadecimal = hex_chars[rem] + hexadecimal 
-        num //= 16 
+        number //= 16 
     print(f"Hexadecimal representation is: {hexadecimal}") 
     return hexadecimal  # Return the hexadecimal value for testing 
 
